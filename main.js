b75a5a93620b520584e1565aaa74d5eda741ee5b
HeatherRouke <heather214@gmail.com>
2024-03-05 13:07:45 -0500

    function to toggle icon

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+/*constants*/
+
+const minusSign = '/assets/images/icon-minus.svg';
+const plusSign = '/assets/images/icon-plus.svg';
+
+
 /*cached elements*/
 
 const Question = document.querySelectorAll('.question');
 const Answer = document.querySelectorAll('.answer');
+const Icon = document.querySelectorAll('.icon');
 
 /*event listener*/
 
@@ -16,6 +23,7 @@ Question.forEach((question,index) => {
 
 function toggleAnswer(index) {
    Answer[index].classList.toggle('show');
+   Icon[index].src = Answer[index].classList.contains('show')? minusSign : plusSign;
 
     }
 
